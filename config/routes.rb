2242cb835c65dcbd511c0082574fbe212d400760
shogo0625 <shogo19940625@gmail.com>
2020-02-13 04:42:30 +0000

    [Finish] 5-5

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
-  end
+end
+	resources :users, only: [:show]
 end
